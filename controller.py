52182273c6c5a538b46765faf3514c9bccc966cc
PaperFlu <1395348685z@gmail.com>
2021-02-07 01:52:44 +0800

    Alpha Support for B-C Buttons

--- controller.py
+++ controller.py
@@ -1,36 +1,68 @@
+import timeit
 import sounddevice as sd
-from media_controls import toggle_play
+from media_controls import control_media_by_id
 
-SAMPLE_RATE = 1000 # Sample rate for our input stream
-BLOCK_SIZE = 100 # Number of samples before we trigger a processing callback
-PRESS_SECONDS = 0.2 # Number of seconds button should be held to register press
-PRESS_SAMPLE_THRESHOLD = 0.9 # Signal amplitude to register as a button press
+SAMPLE_RATE = 1000  # Sample rate for our input stream
+BLOCK_SIZE = 10  # Number of samples before we trigger a processing callback
+LONG_PRESS_SECONDS = 0.6  # Number of seconds button should be held to register long press
 
-BLOCKS_TO_PRESS = (SAMPLE_RATE/BLOCK_SIZE)*PRESS_SECONDS
+BUTTON_A_THRESHOLD = 1
+BUTTON_B_THRESHOLD = 0.025
+BUTTON_C_THRESHOLD = 0.015
+NORMAL_THRESHOLD = 0.01
+
+RECOVER_THRESHOLD = 0.0005
+RECOVER_DEVIATION = 0.001
+
+LOG_FILE = open("log.txt", "a")
+LOG_FILE.write("//\n")
+LOG_FILE.flush()
 
 
 class HeadsetButtonController:
-    def process_frames(self, indata, frames, time, status):
-        mean = sum([y for x in indata[:] for y in x])/len(indata[:])
+    def process_frames(self, in_data):
+        mean1 = sum([x[0] for x in in_data[:]]) / len(in_data[:])
+        mean2 = sum([x[1] for x in in_data[:]]) / len(in_data[:])
 
-        if mean < PRESS_SAMPLE_THRESHOLD:
-            self.times_pressed += 1
+        diff_list = [x[0] - x[1] for x in in_data[:]]
+        deviation = max(diff_list[:]) - min(diff_list[:])
 
-            if self.times_pressed > BLOCKS_TO_PRESS and not self.is_held:
-                toggle_play()
-                self.is_held = True
+        diff = mean2 - mean1
+
+        if self.is_pressing:
+            diff_abs = abs(diff)
+            if diff_abs < RECOVER_THRESHOLD and deviation < RECOVER_DEVIATION:
+                self.is_pressing = False
+
+                if self.largest_diff <= BUTTON_C_THRESHOLD:
+                    control_media_by_id('C')
+                elif self.largest_diff <= BUTTON_B_THRESHOLD:
+                    control_media_by_id('B')
+                elif self.largest_diff <= BUTTON_A_THRESHOLD:
+                    control_media_by_id('A')
+
+                LOG_FILE.write("%f %f %f\n" % (self.largest_diff, timeit.default_timer() - self.press_time, deviation))
+                LOG_FILE.flush()
+
+            else:
+                if self.largest_diff < diff:
+                    self.largest_diff = diff
+
         else:
-            self.is_held = False
-            self.times_pressed = 0
+            if diff > NORMAL_THRESHOLD:
+                self.is_pressing = True
+                self.largest_diff = diff
+                self.press_time = timeit.default_timer()
 
     def __init__(self):
         self.stream = sd.InputStream(
             samplerate=SAMPLE_RATE,
             blocksize=BLOCK_SIZE,
-            channels=1,
+            channels=2,
             callback=self.process_frames
         )
         self.stream.start()
 
-        self.is_held = True
-        self.times_pressed = 0
+        self.is_pressing = False
+        self.largest_diff = 0
+        self.press_time = 0
--- media_controls.py
+++ media_controls.py
@@ -1,7 +1,19 @@
 import win32api
-import win32con
 
 VK_MEDIA_PLAY_PAUSE = 0xB3
+VK_VOLUME_DOWN = 0xAE
+VK_VOLUME_UP = 0xAF
+
+
+def control_media_by_id(button_id):
+    target_vk = {
+        'A': VK_MEDIA_PLAY_PAUSE,
+        'B': VK_VOLUME_UP,
+        'C': VK_VOLUME_DOWN
+    }[button_id]
+
+    win32api.keybd_event(target_vk, 0, 0, 0)
+
 
 def toggle_play():
     win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, 0, 0, 0)
