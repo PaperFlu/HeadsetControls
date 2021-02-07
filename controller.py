0fe87847218d150ad9d19c3cf9d3459f3d591d78
PaperFlu <1395348685z@gmail.com>
2021-02-07 09:28:15 +0800

    More stable long press and button detection.

--- controller.py
+++ controller.py
@@ -7,12 +7,12 @@ BLOCK_SIZE = 10  # Number of samples before we trigger a processing callback
 LONG_PRESS_SECONDS = 0.6  # Number of seconds button should be held to register long press
 
 BUTTON_A_THRESHOLD = 1
-BUTTON_B_THRESHOLD = 0.025
-BUTTON_C_THRESHOLD = 0.015
-NORMAL_THRESHOLD = 0.01
+BUTTON_B_THRESHOLD = 0.028
+BUTTON_C_THRESHOLD = 0.02
+NORMAL_THRESHOLD = 0.015
 
 RECOVER_THRESHOLD = 0.0005
-RECOVER_DEVIATION = 0.001
+RECOVER_DEVIATION = 0.0005
 
 LOG_FILE = open("log.txt", "a")
 LOG_FILE.write("//\n")
@@ -21,13 +21,10 @@ LOG_FILE.flush()
 
 class HeadsetButtonController:
     def process_frames(self, in_data, frames, time, status):
-        mean1 = sum([x[0] for x in in_data[:]]) / len(in_data[:])
-        mean2 = sum([x[1] for x in in_data[:]]) / len(in_data[:])
+        diff_list = [x[1] - x[0] for x in in_data[:]]
 
-        diff_list = [x[0] - x[1] for x in in_data[:]]
+        diff = max(diff_list[:])
         deviation = max(diff_list[:]) - min(diff_list[:])
-
-        diff = mean2 - mean1
 
         if self.is_pressing:
             diff_abs = abs(diff)
@@ -42,9 +39,9 @@ class HeadsetButtonController:
                 LOG_FILE.write("%f %f %f\n" % (self.largest_diff, press_duration, deviation))
                 LOG_FILE.flush()
 
-            if not self.fired:
-                if (is_long_press and not is_recovered) or (is_recovered and not is_long_press):
-                    self.fired = True
+            if not self.is_fired:
+                if is_recovered or is_long_press:
+                    self.is_fired = True
 
                     if self.largest_diff <= BUTTON_C_THRESHOLD:
                         control_media_by_id('C', is_long_press)
@@ -53,7 +50,7 @@ class HeadsetButtonController:
                     elif self.largest_diff <= BUTTON_A_THRESHOLD:
                         control_media_by_id('A', is_long_press)
 
-                if self.largest_diff < diff:
+                elif self.largest_diff < diff:
                     self.largest_diff = diff
 
         else:
@@ -61,7 +58,7 @@ class HeadsetButtonController:
                 self.is_pressing = True
                 self.largest_diff = diff
                 self.press_time = timeit.default_timer()
-                self.fired = False
+                self.is_fired = False
 
     def __init__(self):
         self.stream = sd.InputStream(
@@ -75,4 +72,4 @@ class HeadsetButtonController:
         self.is_pressing = False
         self.largest_diff = 0
         self.press_time = 0
-        self.fired = False
+        self.is_fired = False
